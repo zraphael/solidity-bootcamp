fceeb8e4bbc0064734c97e1c346fcb9d2eef2135
zraphael <raphael@wcarvalho.com>
2022-04-03 19:03:04 -0300

    New functions such as setUserScore and getUserScore

--- score-contract/score.sol
+++ score-contract/score.sol
@@ -3,28 +3,37 @@ pragma solidity ^0.8.0; // That's nice to be fixed when building for production
 
 contract Score {
 
-uint score;
-address admin;
-event ScoreSet(uint);
+    uint score;
+    address admin;
+    mapping(address => uint) public score_list;
+    event ScoreSet(uint);
 
-modifier onlyAdmin {
-    if (msg.sender == admin) {
-        _;
+    modifier onlyAdmin {
+        if (msg.sender == admin) {
+            _;
+        }
     }
-}
 
-constructor () {
-    score = 5;
-    admin = msg.sender;
-}
+    constructor () {
+        score = 5;
+        admin = msg.sender;
+    }
 
-function getScore() public view returns (uint) {
-    return score;
-}
+    function getScore() public view returns (uint) {
+        return score;
+    }
 
-function setScore(uint _newScore) public onlyAdmin {
-    score = _newScore;
-    emit ScoreSet(score);
-}
+    function setScore(uint _newScore) public onlyAdmin {
+        score = _newScore;
+        emit ScoreSet(score);
+    }
+
+    function setUserScore(address _user, uint _newScore) public onlyAdmin {
+        score_list[_user] = _newScore;
+    }
+
+    function getUserScore(address user) public view returns (uint) {
+        return score_list[user];
+    }
 
 }
